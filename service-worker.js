2c728ddeafff1a47d8345cfc7af9c881e4fd729d
mevivekguptaa <me.vivekguptaa@gmail.com>
2026-01-01 08:54:00 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("offline-gps-v1").then(cache => {
+    caches.open("offline-gps-v2").then(cache => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -14,7 +14,15 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request).then(fetchRes => {
+          return caches.open("offline-gps-v2").then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
     })
   );
 });
